17101a92e0b95c8d34d76bced41bf91a0d1e9fe8
TPGPL <TPGPL@users.noreply.github.com>
2024-04-30 00:28:51 +0200

    Initial commit

---
+++ src/main/java/pl/edu/pw/cinemasterbe/CinemasterBeApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.cinemasterbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemasterBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemasterBeApplication.class, args);
+    }
+
+}
