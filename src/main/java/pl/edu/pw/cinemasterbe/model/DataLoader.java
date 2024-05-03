101e6fe08a6ad64ae56a956f30f3a416cca8e07e
TPGPL <TPGPL@users.noreply.github.com>
2024-05-03 02:32:48 +0200

    Seed ticket types

--- src/main/java/pl/edu/pw/cinemasterbe/model/DataLoader.java
+++ src/main/java/pl/edu/pw/cinemasterbe/model/DataLoader.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import pl.edu.pw.cinemasterbe.model.seeders.ScreeningTypeSeeder;
 import pl.edu.pw.cinemasterbe.model.seeders.SeatTypeSeeder;
+import pl.edu.pw.cinemasterbe.model.seeders.TicketTypeSeeder;
 import pl.edu.pw.cinemasterbe.model.seeders.UserSeeder;
 
 @Component
@@ -13,11 +14,13 @@ public class DataLoader implements CommandLineRunner {
     private final UserSeeder userSeeder;
     private final SeatTypeSeeder seatTypeSeeder;
     private final ScreeningTypeSeeder screeningTypeSeeder;
+    private final TicketTypeSeeder ticketTypeSeeder;
 
     @Override
     public void run(String... args) {
         userSeeder.seed();
         seatTypeSeeder.seed();
         screeningTypeSeeder.seed();
+        ticketTypeSeeder.seed();
     }
 }
---
+++ src/main/java/pl/edu/pw/cinemasterbe/model/seeders/TicketTypeSeeder.java
@@ -0,0 +1,24 @@
+package pl.edu.pw.cinemasterbe.model.seeders;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import pl.edu.pw.cinemasterbe.model.domain.TicketType;
+import pl.edu.pw.cinemasterbe.repositories.TicketTypeRepository;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class TicketTypeSeeder {
+    private final TicketTypeRepository ticketTypeRepository;
+
+    public void seed() {
+        var ticketTypes = List.of(TicketType.builder().name("Bilet normalny").description("Bilet dla dorosłych od 18 roku życia.").price(BigDecimal.valueOf(18.12)).build(),
+                TicketType.builder().name("Bilet ulgowy").description("Bilet dla młodzieży do 18 roku życia lub studentów.").price(BigDecimal.valueOf(15.20)).build(),
+                TicketType.builder().name("Bilet grupowy").price(BigDecimal.valueOf(12.10)).build(),
+                TicketType.builder().name("Bilet szkolny").price(BigDecimal.valueOf(9.80)).build());
+
+        ticketTypeRepository.saveAll(ticketTypes);
+    }
+}
