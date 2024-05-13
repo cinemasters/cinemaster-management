6601933761aa42d2d4776a1a59e97f4c8bac7d67
TPGPL <TPGPL@users.noreply.github.com>
2024-05-13 20:27:42 +0200

    add date column to review

--- src/main/java/pl/edu/pw/cinemasterbe/model/domain/Review.java
+++ src/main/java/pl/edu/pw/cinemasterbe/model/domain/Review.java
@@ -3,6 +3,7 @@ package pl.edu.pw.cinemasterbe.model.domain;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,6 +11,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.time.Instant;
 
 @Entity
 @Table(name = "Review")
@@ -26,6 +28,9 @@ public class Review {
     @Min(0)
     @Max(5)
     private BigDecimal rating;
+    @NotNull
+    @Column(name = "publication_date")
+    private Instant publicationDate;
     @Column(name = "is_hidden")
     private boolean hidden;
     @ManyToOne(optional = false)
