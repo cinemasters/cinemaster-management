fe0e741c212163fb3975b48fc716386d5f4b5e52
TPGPL <TPGPL@users.noreply.github.com>
2024-05-19 00:08:31 +0200

    simplify room layout dto mapping

--- src/main/java/pl/edu/pw/cinemasterbe/model/mappers/RoomLayoutMapper.java
+++ src/main/java/pl/edu/pw/cinemasterbe/model/mappers/RoomLayoutMapper.java
@@ -1,6 +1,8 @@
 package pl.edu.pw.cinemasterbe.model.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import pl.edu.pw.cinemasterbe.model.domain.cinema.LayoutSeat;
 import pl.edu.pw.cinemasterbe.model.domain.cinema.RoomLayout;
 import pl.edu.pw.cinemasterbe.model.dto.cinema.LayoutSeatDto;
 import pl.edu.pw.cinemasterbe.model.dto.cinema.RoomLayoutDetailsDto;
@@ -9,18 +11,10 @@ import pl.edu.pw.cinemasterbe.model.dto.cinema.RoomLayoutGridDto;
 @Mapper(componentModel = "spring")
 public interface RoomLayoutMapper {
     RoomLayoutGridDto mapToGridDto(RoomLayout entity);
+    
+    RoomLayoutDetailsDto mapToDetailsDto(RoomLayout entity);
 
-    default RoomLayoutDetailsDto mapToDetailsDto(RoomLayout entity) {
-        return RoomLayoutDetailsDto.builder()
-                .name(entity.getName())
-                .rowCount(entity.getRowCount())
-                .columnCount(entity.getColumnCount())
-                .seats(entity.getSeats().stream().map((el) -> LayoutSeatDto.builder()
-                        .col(el.getColumn())
-                        .row(el.getRow())
-                        .hidden(el.isHidden())
-                        .code(el.getType() == null ? null : el.getType().getCode())
-                        .build()).toList())
-                .build();
-    }
+    @Mapping(target = "col", source = "column")
+    @Mapping(target = "code", source = "type.code")
+    LayoutSeatDto mapToSeatDto(LayoutSeat entity);
 }
